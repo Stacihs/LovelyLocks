1d063e7af2b1794d58446263a0f4c5970617aaf5
stacihs <stacihs@hotmail.com>
2017-03-23 15:42:08 -0500

    Add ProducrtAdminViewModel and Controller

--- LovelyLocksFinal/src/LovelyLocks/Controllers/ProductAdminController.cs
+++ LovelyLocksFinal/src/LovelyLocks/Controllers/ProductAdminController.cs
@@ -5,26 +5,70 @@ using Microsoft.EntityFrameworkCore;
 using LovelyLocks.Data;
 using LovelyLocks.Models;
 using Microsoft.AspNetCore.Authorization;
+using System;
+using LovelyLocks.Infrastructure;
 
 namespace LovelyLocks.Controllers
 {
-    [Authorize]
-    public class ProductAdminController : Controller
+    public class ProductsController : Controller
     {
         private readonly ProductDbContext dbcontext;
 
-        public ProductAdminController(ProductDbContext context)
+        public ProductsController(ProductDbContext context)
         {
-            dbcontext = context;    
+            dbcontext = context;
         }
 
-        // GET: ProductAdmin
-        public async Task<IActionResult> Index()
+        // GET: Products
+        public async Task<IActionResult> Index(string sortOrder, string currentFilter,
+            string searchString, int? page)
         {
-            return View(await dbcontext.Products.ToListAsync());
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["CategorySortParm"] = String.IsNullOrEmpty(sortOrder) ? "category_desc" : "";
+            ViewData["NameSortParm"] = sortOrder == "Name" ? "name_desc" : "Name";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewData["CurrentFilter"] = searchString;
+
+            var products = from p in dbcontext.Products
+                           select p;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                products = products.Where(p => p.Name.Contains(searchString));
+
+            }
+            switch (sortOrder)
+            {
+                case "category_desc":
+                    products = products.OrderByDescending(p => p.Category);
+                    break;
+                case "Name":
+                    products = products.OrderBy(p => p.Name);
+                    break;
+                case "name_desc":
+                    products = products.OrderByDescending(p => p.Name);
+                    break;
+                default:
+                    products = products.OrderBy(p => p.Category);
+                    break;
+            }
+
+            int pageSize = 4;
+            //return View(await products.AsNoTracking().ToListAsync());
+            return View(await PaginatedList<Product>.CreateAsync(products.AsNoTracking(), page ?? 1, pageSize));
+
+
         }
 
-        // GET: ProductAdmin/Details/5
+        // GET: Products/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -41,13 +85,13 @@ namespace LovelyLocks.Controllers
             return View(product);
         }
 
-        // GET: ProductAdmin/Create
+        // GET: Products/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: ProductAdmin/Create
+        // POST: Products/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
@@ -63,7 +107,7 @@ namespace LovelyLocks.Controllers
             return View(product);
         }
 
-        // GET: ProductAdmin/Edit/5
+        // GET: Products/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -79,7 +123,7 @@ namespace LovelyLocks.Controllers
             return View(product);
         }
 
-        // POST: ProductAdmin/Edit/5
+        // POST: Products/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
@@ -114,7 +158,7 @@ namespace LovelyLocks.Controllers
             return View(product);
         }
 
-        // GET: ProductAdmin/Delete/5
+        // GET: Products/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -131,7 +175,7 @@ namespace LovelyLocks.Controllers
             return View(product);
         }
 
-        // POST: ProductAdmin/Delete/5
+        // POST: Products/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
---
+++ LovelyLocksFinal/src/LovelyLocks/Models/ViewModels/ProductAdminViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LovelyLocks.Models.ViewModels
+{
+    public class ProductAdminViewModel
+    {
+
+        public int ProductID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public string Category { get; set; }
+    }
+}
